9ea4ad444a2f7aa743220041307fa67098e022e7
Guido Hovens <guido@hovensict.nl>
2018-08-08 22:19:22 +0200

    Overlay and such

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-// Select DOM Items
+// Selecteer DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-// Set Initial State Of menu
+// De initiële staat van het menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,7 +18,7 @@ function toggleMenu() {
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
-    // Set Menu State
+    // Verander menu staat
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
@@ -27,7 +27,7 @@ function toggleMenu() {
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
-    // Set Menu State
+    // Verander menu staat
     showMenu = false;
   }
 }
